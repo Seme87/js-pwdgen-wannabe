7f1eff2b75ebc46e61f6c0cebda85e64c9c7e913
Seme87 <massimomonteleone@hotmail.com>
2022-10-19 15:12:59 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//CHIEDO IL NOME ALL'UTENTE
+const firstName = prompt ('Inserisci il tuo nome');
+
+//CHIEDO IL COGNOME ALL'UTENTE
+const lastName = prompt ('Inserisci il tuo cognome');
+
+//CHIEDO IL COLORE PPREFITO
+const favoriteColor = prompt ('Inserisci il tuo colore preferito');
+
+// NUMERO DELLA CLASSE
+const numberClass = (76);
+
+//WELCOME MESSAGE
+const welcomeMessage = (`${firstName}Massim ${lastName}, ${favoriteColor}, ${numberClass}`)
+
+//STAMPA SULLA PAGINA
+document.querySelector('h1').innerHTML = welcomeMessage;
